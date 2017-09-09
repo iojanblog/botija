3e299b0759ec7e4affc5ade193e755ca72001a69
iojanblog <jmedinap@gmail.com>
2017-09-09 11:38:32 -0500

    Dealing with horrible nodejs installation troubles

--- plug.august.sh
+++ plug.august.sh
@@ -9,22 +9,27 @@ source `dirname $0`/botija.init.sh
 # install logic
 function install {
     [ -z "$1" ] && echo "${bl_missing_argument}: offlineKey" && return 1
-
-    curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-    sudo apt-get install nodejs && sudo chown -R $(whoami) /usr/{lib/node_modules,bin,share}
+    command -v node || install_nodejs
 
     npm install -g bluetooth-hci-socket augustctl
-    echo "{ \"offlineKey\": \"${1}\", \"offlineKeyOffset\": 1 }" > /usr/lib/node_modules/augustctl/config.json
     sudo setcap cap_net_raw+eip $(readlink -f `which node`)
+    echo "{ \"offlineKey\": \"${1}\", \"offlineKeyOffset\": 1 }" > $(npm config get prefix)/lib/node_modules/augustctl/config.json
 
     augustctl lock   
     $local_dir/botija.sh send_text "$bl_august_install ($?)"
 }
 
 
+function install_nodejs {
+    curl https://nodejs.org/dist/v7.8.0/node-v7.8.0-linux-armv7l.tar.xz | tar xvz
+    mv node-v7.8.0-linux-armv7l $tmp_dir/nodejs
+}
+
+
 #
 # lock logic
 function lock {
+    command -v node >/dev/null 2>&1 || export PATH=$tmp_dir/nodejs/bin:$PATH
     timeout --preserve-status $delay_timeout augustctl lock
     $local_dir/botija.sh send_text "$bl_door_lock ($?)"
 }
@@ -33,6 +38,7 @@ function lock {
 #
 # unlock logic
 function unlock {
+    command -v node >/dev/null 2>&1 || export PATH=$tmp_dir/nodejs/bin:$PATH
     timeout --preserve-status $delay_timeout augustctl unlock
     $local_dir/botija.sh send_text "$bl_door_unlock ($?)"
 }
