107d43cc7fb47d7fe5377f7c69323fca8aa3da4a
iojanblog <jmedinap@gmail.com>
2017-09-08 19:13:21 -0500

    upgrade bug fix

--- plug.upgrade.sh
+++ plug.upgrade.sh
@@ -3,6 +3,5 @@
 # Status plugin
 #
 source `dirname $0`/botija.init.sh
-
-`cd $local_dir && git pull`
+cd $local_dir && git pull >/dev/null 2>&1
 $local_dir/botija.sh send_text "$bl_upgrade_finished ($?)"
