31c7bb327f67f5f70ee6086b1ef25870fdbe6313
Johann M <jmedinap@gmail.com>
2017-10-29 19:10:20 -0500

    cleanup

--- plug.camera.callback.sh
+++ plug.camera.callback.sh
@@ -12,9 +12,10 @@ if [ "$event_id" = "video" ]; then
     [ "$?" -ne "0" ] && $local_dir/botija.sh send_text "$bl_failed_avconv" && exit 1
 
     $local_dir/botija.sh send_video ${event_arg}.mp4
-    rm -f ${event_arg}.mp4
+    rm -f ${event_arg}.mp4 ${event_arg}
 elif [ "$event_id" = "photo" ]; then
     $local_dir/botija.sh send_photo ${event_arg}
+    rm -f ${event_arg}
 else
     $local_dir/botija.sh send_text "$event_id $bl_event_unknown"
 fi
